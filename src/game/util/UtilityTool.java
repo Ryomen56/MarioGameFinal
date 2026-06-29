0b0bbbff00c98e20380b0db93ae15669ed857cca
Rio Gus Erlandy <rioguserlandy89534@gmail.com>
2026-06-29 23:53:14 +0700

    MODUL 2 - Utility Tool(Selesai)

--- src/game/util/UtilityTool.java
+++ src/game/util/UtilityTool.java
@@ -1,15 +1,24 @@
 package game.util;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Utility untuk membaca dan mengolah gambar.
+ */
 public final class UtilityTool {
 
     private UtilityTool() {
     }
+
+    // ==========================================
+    // LOAD IMAGE
+    // ==========================================
 
     public static BufferedImage loadImage(String path) {
 
@@ -20,12 +29,43 @@ public final class UtilityTool {
         } catch (IOException e) {
 
             throw new RuntimeException(
-
-                    "Tidak dapat membaca file:\n" + path,
-
-                    e);
-
+                    "Gagal membaca gambar : " + path);
         }
+
+    }
+
+    // ==========================================
+    // SCALE IMAGE
+    // ==========================================
+
+    public static BufferedImage scaleImage(
+            BufferedImage original,
+            int width,
+            int height) {
+
+        BufferedImage scaledImage =
+                new BufferedImage(
+                        width,
+                        height,
+                        BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2 = scaledImage.createGraphics();
+
+        g2.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+
+        g2.drawImage(
+                original,
+                0,
+                0,
+                width,
+                height,
+                null);
+
+        g2.dispose();
+
+        return scaledImage;
 
     }
 
